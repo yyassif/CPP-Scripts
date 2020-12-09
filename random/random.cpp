2debee9149725e84e801c2f5bfbb06598484507b
Yassine YASSIF <yess.mapss@gmail.com>
2020-12-09 21:00:22 +0100

    Update random.cpp

--- random/random.cpp
+++ random/random.cpp
@@ -1,25 +1,18 @@
 #include <cstdlib>
 #include <iostream>
-#include <string>
 #include <cmath>
 #include <ctime>
-#include <limits>
 #include <stdio.h>
 using namespace std;
 
 template <typename T>
 T random(T a = 0, T b = 1, bool is_int = false) {
     srand(time(NULL));
-    double arbitary = 0;
-    while (1) {
-        arbitary = fmod(rand(), b);
-        if (a <= arbitary && arbitary <= b) {
-            if (is_int) {
-                return int(arbitary);
-            } else {
-                return arbitary;
-            }
-        }
+    double arbitary = fmod(rand(), b - a);
+    if (is_int) {
+        return int(arbitary + a);
+    } else {
+        return arbitary + a;
     }
 }
 
